de8542cb3990cb8fd431767ef8946488d53a9a72
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-11 21:26:49 +0300

    test

--- src/test/java/com/automation/runner/TestRunner.java
+++ src/test/java/com/automation/runner/TestRunner.java
@@ -1,6 +1,7 @@
 package com.automation.runner;
 
 
+import com.automation.testcases.CartPageTest;
 import com.automation.testcases.LoginTest;
 import com.automation.testcases.SearchFunctionalityTest;
 import org.junit.runner.RunWith;
@@ -8,7 +9,7 @@ import org.junit.runners.Suite;
 import org.w3c.dom.ls.LSOutput;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({LoginTest.class, SearchFunctionalityTest.class})
+@Suite.SuiteClasses({CartPageTest.class,LoginTest.class, SearchFunctionalityTest.class})
 public class TestRunner {
 
 }
